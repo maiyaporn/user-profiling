acc9faf594380f80b8a1f26c7807b37ba0cba519
maiyaporn <p.maiyaporn@gmail.com>
2014-12-14 19:35:16 -0500

    lda training and evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,4 @@
-from gensim import corpora, models, similarities
+from gensim import corpora, models, similarities, matutils
 import numpy as np
 from pandas import *
 import matplotlib.pyplot as plt
@@ -10,6 +10,8 @@ from scipy.stats.stats import pearsonr
 
 class Evaluation(object):
 	"""docstring for Evaluation"""
+	def __init__(self):
+		self.train_corpus = corpora.MmCorpus('tmp/train-corpus.mm')
 
 	def loadLdaModel(self, n):
 		model = 'tmp/lda_n' + str(n) + '_testing_corpus.lda'
@@ -28,7 +30,7 @@ class Evaluation(object):
 		directory = 'label/' + str(uid) + '_bus.csv'
 		return self.readCsvToList(directory)
 
-	# Calculate pearson correlation score	
+	# Calculate pearson correlation score
 	def getCorrelation(self, data, uid, sid):
 		u_res = data[data['user_id'] == uid]
 		s_res = data[data['user_id'] == sid]
@@ -60,7 +62,7 @@ class Evaluation(object):
 		data = DataFrame(read_json('data50.json'))
 
 		if os.path.isfile(pathToIndex):
-			index = similarities.MatrixSimilarity.load(pathToIndex)
+			index = similarities.Similarity.load(pathToIndex)
 		else:
 			model = self.loadLdaModel(n)
 			# Transform corpus to lda space and Build an index it - the order of document should be preserved
@@ -88,15 +90,33 @@ class Evaluation(object):
 					wr2.writerow([sid, index.vector_by_id(sdocno)])
 				wr1.writerow([uid, sid, score] + corr)
 
+	def hellinger(self, vec1, vec2):
+		return np.sqrt(0.5 * ((vec1 - vec2)**2).sum())
+
+	def getSimilaritiesByHellinger(self, n):
+		# Load testing corpus
+		test_corpus = corpora.MmCorpus('tmp/test-corpus.mm')
+		doc_idx_map = self.loadFromPickle('tmp/doc-idx-map')
+		data = DataFrame(read_json('data50.json'))
+
+		simFile = open('tmp/sim_hl_' + str(n) + '.csv', 'wb')
+		wr1 = csv.writer(simFile)
+
+		model = self.loadLdaModel(n)
+		precomputed_vec2s = np.sqrt(matutils.corpus2dense(model[test_corpus], num_terms=n, num_docs=len(test_corpus)).transpose())
+		for vid, vdoc in enumerate(precomputed_vec2s):
+			sims = [(tid, self.hellinger(vdoc, tdoc)) for tid, tdoc in enumerate(precomputed_vec2s)]
+			sims = sorted(sims, key=lambda item: item[1])
+			uid = doc_idx_map.get(vid)
+			for sdocno, score in sims[1:6]:
+				sid = doc_idx_map.get(sdocno)
+				corr = self.getCorrelation(data, uid, sid)
+				wr1.writerow([uid, sid, score] + corr)
+
 	def getTopics(self,n):
 		model = self.loadLdaModel(n)
 		# Save topics distribution in a file
-		topics = model.show_topics(n, num_words=100, formatted=True)
 		topics_tuple = model.show_topics(n, num_words=100, formatted=False)
-		with open( 'tmp/' + str(n) + '_topics.txt', 'wb') as f1:
-			for t in topics:
-				f1.write("%s\n" % t.encode('utf-8'))
-
 		with open( 'tmp/' + str(n) + '_topics', 'wb') as f2:
 			pickle.dump(topics_tuple, f2)
 
@@ -104,9 +124,10 @@ def main():
 	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 	e = Evaluation()
 
-	for n in [60,120]:
-		e.getTopics(n)
-		e.getSimilarities(n, 'tmp/index/index_n' + str(n))
+	for n in [60,120,220,340]:
+		#e.getTopics(n)
+		#e.getSimilarities(n, 'tmp/index/index_n' + str(n))
+		e.getSimilaritiesByHellinger(n)
 
 if __name__ == '__main__':
 	main()
